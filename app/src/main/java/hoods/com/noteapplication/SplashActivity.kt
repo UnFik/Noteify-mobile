1e723dd54e07b23863c690e028f25fa89435ba24
UnFik <fikriilhamarifin_1313621027@mhs.unj.ac.id>
2023-12-28 20:02:27 +0700

    first commit

---
+++ app/src/main/java/hoods/com/noteapplication/SplashActivity.kt
@@ -0,0 +1,63 @@
+package hoods.com.noteapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.Animatable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import hoods.com.noteapplication.ui.theme.NoteApplicationTheme
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+            NoteApplicationTheme {
+                SplashScreen()
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun SplashScreen() {
+        val alpha = remember {
+            androidx.compose.animation.core.Animatable(0f)
+        }
+        LaunchedEffect(key1 = true){
+            alpha.animateTo(1f)
+            delay(2000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(painter = painterResource(id = R.drawable.unj), contentDescription = "Splash Screen", modifier = Modifier.size(200.dp))
+            Text(text = "Note Application", modifier = Modifier.padding(top = 16.dp))
+            Text(text = "Fikri Ilham Arifin", fontSize = 20.sp, modifier = Modifier.padding(top = 16.dp))
+            Text(text = "NIM : 1313621027", fontSize = 20.sp)
+
+        }
+    }
+}
+
